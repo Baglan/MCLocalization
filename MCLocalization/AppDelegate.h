64749639775a2f0be80a5471afdb34b0ae3e2e56
Baglan Dosmagambetov <baglan.dos@gmail.com>
2013-03-14 15:12:45 +0600

    Initial Commit

---
+++ MCLocalization/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MCLocalization
+//
+//  Created by Baglan on 3/14/13.
+//  Copyright (c) 2013 MobileCreators. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ MCLocalization/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MCLocalization
+//
+//  Created by Baglan on 3/14/13.
+//  Copyright (c) 2013 MobileCreators. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
